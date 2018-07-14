f7e876697d53db5dbe49acbdd961d987e3d8c657
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-07-14 15:33:08 +0200

    Initial commit

---
+++ Assets/Scripts/GenerateSegment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateSegment : MonoBehaviour {
+
+	public int width =  10;
+	public int height = 10;
+
+}
